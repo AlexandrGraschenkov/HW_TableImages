fb1ad026430e10f90de67c536db4353696c94904
leonspok <igor9774@gmail.com>
2014-04-29 11:36:33 +0400

    Task completed

--- HW_TableImages/MyTableViewController.m
+++ HW_TableImages/MyTableViewController.m
@@ -13,6 +13,8 @@
 @interface MyTableViewController ()
 {
     NSArray *dataArr;
+    
+    NSMutableArray *cells;
 }
 @end
 
@@ -22,6 +24,24 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    dataArr = [NSArray array];
+    
+    cells = [NSMutableArray new];
+    
+    [[DataManager sharedInstance] asyncListOfFruits:^(NSArray *arr) {
+        dataArr = [NSArray arrayWithArray:arr];
+        
+        NSMutableArray *newCellsPaths = [NSMutableArray array];
+        for (NSInteger i = 0; i < dataArr.count; i++) {
+            NSIndexPath *indexPath = [NSIndexPath indexPathForRow:i inSection:0];
+            [newCellsPaths addObject:indexPath];
+        }
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self.tableView insertRowsAtIndexPaths:newCellsPaths withRowAnimation:UITableViewRowAnimationMiddle];
+        });
+    }];
 }
 
 
@@ -29,21 +49,31 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-#warning Incomplete method implementation.
-    // Return the number of rows in the section.
-    return 0;
+    return dataArr.count;
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     TableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"TableViewCell" forIndexPath:indexPath];
     
-    // Configure the cell...
+    [cell.titleLabel setText:[[dataArr objectAtIndex:indexPath.row] objectForKey:@"title"]];
+    
+    [cell.activity startAnimating];
+    [cell.imgView setImage:nil];
+    
+    [[DataManager sharedInstance] asyncGetImage:[[dataArr objectAtIndex:indexPath.row] objectForKey:@"thumb_img"] complection:^(UIImage *img) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            NSIndexPath *newPath = [tableView indexPathForCell:cell];
+            if ([newPath isEqual:indexPath]) {
+                [cell.activity stopAnimating];
+                [cell.imgView setImage:img];
+            }
+        });
+    }];
     
     return cell;
 }
-*/
 
 
 @end
