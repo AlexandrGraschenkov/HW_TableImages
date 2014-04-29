d2d3c88fe448399c9862438be51e105fccccbc52
nastya13 <nastya94-94.08@mail.ru>
2014-04-28 18:18:17 -0700

    done

--- HW_TableImages/MyTableViewController.m
+++ HW_TableImages/MyTableViewController.m
@@ -12,6 +12,7 @@
 
 @interface MyTableViewController ()
 {
+    DataManager *data;
     NSArray *dataArr;
 }
 @end
@@ -22,28 +23,49 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    data = [DataManager sharedInstance];
+    [data asyncListOfFruits:^(NSArray *arr) {
+        dataArr = arr;
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self.tableView reloadData];
+        });
+    }];
+    
+    
 }
-
 
 #pragma mark - Table view data source
-
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-#warning Incomplete method implementation.
-    // Return the number of rows in the section.
-    return 0;
+    return dataArr.count;
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    TableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"TableViewCell" forIndexPath:indexPath];
+   static NSString *cellIdentifier = @"TableViewCell";
+   TableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier forIndexPath:indexPath];
+    [cell.activity startAnimating];
+    cell.titleLabel.text = [[dataArr objectAtIndex:indexPath.row] objectForKey:@"title"];
+    cell.imgView.image = nil;
     
-    // Configure the cell...
-    
+    [data asyncGetImage:[[dataArr objectAtIndex:indexPath.row]objectForKey:@"thumb_img"] complection:^void (UIImage *img)
+    {
+        TableViewCell *cell = (TableViewCell *)[self.tableView cellForRowAtIndexPath:indexPath];
+        if (cell)
+        {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                cell.imgView.image = img;
+                [cell.activity setHidesWhenStopped:YES];
+                [cell.activity stopAnimating];
+            });
+        }
+        	
+    }];
     return cell;
+    
 }
-*/
+
 
 
 @end
